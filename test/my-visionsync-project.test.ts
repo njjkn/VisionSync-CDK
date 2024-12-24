e8fa9422a1f35f77afc83e6ac034af38ca3f1e59
njjkjn <deadender91@gmail.com>
2024-12-24 10:34:24 -0800

    a

---
+++ test/my-visionsync-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyVisionsyncProject from '../lib/my-visionsync-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-visionsync-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyVisionsyncProject.MyVisionsyncProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
